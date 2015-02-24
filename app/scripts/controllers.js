bfcb59449281778e45285f33d5cf312e22d530b6
GrantSchoffelen <grant.schoffelen@gmail.com>
2015-02-23 21:37:11 -0500

    so it begins

---
+++ app/scripts/controllers.js
@@ -0,0 +1,39 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, $http, $cordovaGeolocation) {
+
+
+    $cordovaGeolocation
+        .getCurrentPosition()
+        .then(function(position) {
+            console.log(position)
+                })
+            })
+     
+
+
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
